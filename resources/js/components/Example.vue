1920b60dff6435409d96ca187744db52ee8840cc
Samuel Ideapad 3 <samuca@samuca.com>
2022-07-12 00:17:11 -0300

    Pre install vite + vuejs

---
+++ resources/js/components/Example.vue
@@ -0,0 +1,15 @@
+<template> <div>asdasdasdasdasd</div> </template>
+<script>
+export default {
+    name: "Boleto Detail",
+    props: {},
+    components: {},
+    mixins: [],
+    data() {
+        return {};
+    },
+    created() {},
+    methods: {},
+};
+</script>
+<style lang="scss"></style>
--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-$a = 1;
-
 Route::get('/', function () {
-    $a = 1;
-
-    dd(phpinfo());
-
     return view('welcome');
 });
